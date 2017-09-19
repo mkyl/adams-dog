9c909e9fc08e40a43ff645abfc7a2b56a185d56c
mkyl <me@kayali.io>
2017-09-19 09:50:24 -0700

    fixed bug in server

--- server/coordinator.py
+++ server/coordinator.py
@@ -14,14 +14,23 @@ number_manager = number_manager.NumberManager()
 class MyServer(BaseHTTPRequestHandler):
     def do_GET(self):
         if self.path == "/":
-            self.send_response(200)
+            self.protocol_version='HTTP/1.1'
+            self.send_response(200, 'OK')
+            self.send_header('Content-type', 'text/html')
+            self.end_headers()
             message = number_manager.get_a_unknown_number_index()
             self.wfile.write(bytes("http://www.countablethoughts.com/concat-then-factor/dl.py?%s" % message , "utf-8"))
         elif self.path == "/status" or self.path == "/status/":
-            self.send_response(200)
+            self.protocol_version='HTTP/1.1'
+            self.send_response(200, 'OK')
+            self.send_header('Content-type', 'text/html')
+            self.end_headers()
             self.wfile.write(bytes("%d out of 1513 numbers are still unknown" % number_manager.how_many_known(), "utf-8"))
         else:
+            self.protocol_version='HTTP/1.1'
             self.send_response(404)
+            self.send_header('Content-type', 'text/html')
+            self.end_headers()
             self.wfile.write(bytes("File not found\n", "utf-8"))
 
     def do_POST(self):
